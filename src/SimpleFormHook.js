57f7a4d8835ef2808bf0eb32508cda1a67e0c445
shivam <shivampatel27102001@gmail.com>
2021-02-07 11:11:51 +0530

    value reset button added

--- src/SimpleFormHook.js
+++ src/SimpleFormHook.js
@@ -10,7 +10,9 @@ function SimpleFormHook() {
         <div>
             <h1>The value is {email}</h1>
             <input type="text" value={email} onChange={handleChange}/>
+            <button onClick={()=>{setEmail("");}}>Submit</button>
         </div>
+        
     )
 }
 
