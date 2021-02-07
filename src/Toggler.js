da5e837a0c20c7076489b65d96843a4da1b85cfd
shivam <shivampatel27102001@gmail.com>
2021-02-07 10:48:24 +0530

    custom use

--- src/Toggler.js
+++ src/Toggler.js
@@ -1,14 +1,13 @@
 import React, {useState} from 'react';
+import useToggle from './hooks/useToggle';
 
 function Toggler() {
-    const [isHappy,setIsHappy] = useState(true);
-    const [isSingle,setIsSingle] = useState(false);
-    const toggleSingle = () => {
-        setIsSingle(!isSingle);
-    }
-    return (
+    const [isHappy,toggleIsHappy] = useToggle(true);
+    const [isSingle, toggleSingle] = useToggle(false);
+    
+    return( 
         <div>
-<h1 onClick={()=> setIsHappy(!isHappy)}>{isHappy ? "I am happy" : "I am sad"}</h1>
+<h1 onClick={toggleIsHappy}>{isHappy ? "I am happy" : "I am sad"}</h1>
 
             
 <h1 onClick={toggleSingle}>{isSingle ? "I am single" : "I am Married"}</h1>
