85293680ef9100bcd1b43d70371f7b01d5b22d83
shivam <shivampatel27102001@gmail.com>
2021-02-07 10:34:32 +0530

    on branch custoomhook two toggle define

--- src/Toggler.js
+++ src/Toggler.js
@@ -2,9 +2,16 @@ import React, {useState} from 'react';
 
 function Toggler() {
     const [isHappy,setIsHappy] = useState(true);
+    const [isSingle,setIsSingle] = useState(false);
+    const toggleSingle = () => {
+        setIsSingle(!isSingle);
+    }
     return (
         <div>
-            <h1 onClick={()=> setIsHappy(!isHappy)}>{isHappy ? "I am happy" : "I am sad"}</h1>
+<h1 onClick={()=> setIsHappy(!isHappy)}>{isHappy ? "I am happy" : "I am sad"}</h1>
+
+            
+<h1 onClick={toggleSingle}>{isSingle ? "I am single" : "I am Married"}</h1>
         </div>
     )
 }
